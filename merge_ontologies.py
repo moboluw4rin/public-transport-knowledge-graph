6ffd39dff8a195ff42a67dddb28959c93feda529
Aldrich Fernandes <aldrich.fernandes05@gmail.com>
2026-04-21 10:14:06 +0100

    feat: created complete ontology file for submission

---
+++ merge_ontologies.py
@@ -0,0 +1,18 @@
+from rdflib import Graph
+from pathlib import Path
+
+def merge_ontologies():
+    basePath = Path(__file__).parent
+    tboxPath = basePath / "ontologies" / "tbox_ontology.ttl"
+    instancesPath = basePath / "ontologies" / "instances.ttl"
+    outputPath = basePath / "ontologies" / "full_knowledge_graph.ttl"
+
+    g = Graph()
+
+    g.parse(tboxPath, format="turtle")
+    g.parse(instancesPath, format="turtle")
+
+    g.serialize(destination=outputPath, format="longturtle")
+
+if __name__ == "__main__":
+    merge_ontologies()
